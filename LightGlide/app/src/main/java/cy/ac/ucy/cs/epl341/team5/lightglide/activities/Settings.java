c9c81d72749106b10e0c815e0cc0114fb376b055
adamos2468 <adamos2468@gmail.com>
2018-11-14 16:28:51 +0200

    Added the back button for Settings

--- LightGlide/app/src/main/java/cy/ac/ucy/cs/epl341/team5/lightglide/activities/Settings.java
+++ LightGlide/app/src/main/java/cy/ac/ucy/cs/epl341/team5/lightglide/activities/Settings.java
@@ -1,9 +1,20 @@
 package cy.ac.ucy.cs.epl341.team5.lightglide.activities;
 
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import cy.ac.ucy.cs.epl341.team5.lightglide.R;
 
@@ -11,12 +22,21 @@ public class Settings extends PreferenceActivity implements SharedPreferences.On
     static int themeID=R.style.AppTheme;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        LinearLayout root = (LinearLayout)findViewById(android.R.id.list).getParent().getParent().getParent();
+        Toolbar bar = (Toolbar) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(bar, 0); // insert at top
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
         this.setTheme(themeID);
         addPreferencesFromResource(R.xml.preference);
         PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
     }
-
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if(key.equals("theme")) {
